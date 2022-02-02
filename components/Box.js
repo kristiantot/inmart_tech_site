c01b11645191dbcf0a135dd00760fb96cf256f26
Krisztian <kristian88tot@gmail.com>
2022-02-02 23:35:47 +0100

    css edit, layout optimization. Hover function

--- components/Box.js
+++ components/Box.js
@@ -1,8 +1,7 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useMemo } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Box as NativeBox, Html } from "@react-three/drei";
-import { Vector3 } from "three";
-import { Tween } from "three";
+import { Box as NativeBox, Html, useTexture } from "@react-three/drei";
+import * as THREE from "three";
 
 export default function Box(props) {
   const mesh = useRef();
@@ -12,53 +11,37 @@ export default function Box(props) {
 
   useFrame(() => {
     mesh.current.position.lerp(
-      new Vector3(props.position[0], props.position[1], props.endPos),
+      new THREE.Vector3(props.position[0], props.position[1], props.endPos),
       0.05
     );
-    //   let speedMultiplier = 0;
-    //   let speed = 0;
-    // speedMultiplier -= 0.5
-
-    // if(mesh.current.position.z > 10.0)
-    // {
-
-    //    speed = 0.2 * -speedMultiplier;
-    // }
-    // mesh.current.position.z -= speed;
-
-    //console.log(mesh.current.position.z);
+    if(hovered)
+    {
+      mesh.current.position.z += 0.15;
+    }
   });
-
+  const texture = useMemo(() => new THREE.TextureLoader().load(props.logo), []);
   return (
     <NativeBox
       args={[1, 1, 1]}
       {...props}
       ref={mesh}
-      scale={[10, 12, 0.2]}
+      castShadow={true}
+      scale={[10, 10, 0.2]}
       //scale={active ? [6, 6, 6] : [5, 5, 5]}
       onClick={() => setActive(!active)}
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
     >
-      <meshStandardMaterial
+      <meshPhysicalMaterial
+        map={texture}
         attach="material"
-        transparent="true"
-        opacity={0.2}
-        color={"rgba(127,255,255)"}
+        color={"rgb(120,120,120)"}
+        clearcoat={0.2}
+        metalness={0.1}
+        opacity={0.8}
+        transparent={true}
+        clearcoatRoughness={0.4}
       />
-      <Html
-        style={{
-          transition: "all 0.2s",
-          opacity: 0.8,
-          transform: `scale(5)`,
-        }}
-        distanceFactor={1.5}
-        position={[0, 0, 0.51]}
-        transform
-        occlude
-      >
-        <span>Size</span>
-      </Html>
     </NativeBox>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import './index.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
